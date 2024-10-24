fc01683dfda8901d68e83888896d9f877519ea7c
Daniel Klement <info@danielklement.net>
2024-10-24 22:05:33 +0200

    Documentation

--- Daniel-Klement.Enums/Socials/SocialMedia.cs
+++ Daniel-Klement.Enums/Socials/SocialMedia.cs
@@ -1,14 +1,48 @@
 ﻿namespace Daniel_Klement.Enums.Socials
 {
+    /// <summary>
+    /// Tzpe of the social media
+    /// </summary>
     public enum SocialMedia
     {
+        /// <summary>
+        /// Facebook social type
+        /// </summary>
         Facebook,
+
+        /// <summary>
+        /// Instagram social type
+        /// </summary>
         Instagram,
+
+        /// <summary>
+        /// YouTube social type
+        /// </summary>
         YouTube,
+
+        /// <summary>
+        /// Twitter social type
+        /// </summary>
         Twitter,
+
+        /// <summary>
+        /// LinkedIn social type
+        /// </summary>
         LinkedIn,
+
+        /// <summary>
+        /// OneX social type
+        /// </summary>
         OneX,
+
+        /// <summary>
+        /// GuruShots social type
+        /// </summary>
         GuruShots,
+
+        /// <summary>
+        /// SuperPatch social type
+        /// </summary>
         SuperPatch,
     }
 }
