ad70e0fd2f2f66b5fd2a0a6d929ad317e2ef3e94
Daniel Klement <57287296+musakai@users.noreply.github.com>
2024-11-02 16:43:34 +0100

    Created Image Slider Component (#13)
    * Created Image Slider Component
    
    * Added timer into the Slider

---
+++ Musakai.Blazor/Components/Images/ImageSlider/ImageSliderContent.cs
@@ -0,0 +1,23 @@
+﻿namespace Musakai.Blazor.Components.Images.ImageSlider
+{
+    /// <summary>
+    /// Image slider content.
+    /// </summary>
+    public class ImageSliderContent
+    {
+        /// <summary>
+        /// Image source in URI.
+        /// </summary>
+        public required string Src { get; set; }
+
+        /// <summary>
+        /// Image and slide title.
+        /// </summary>
+        public string? Title { get; set; }
+
+        /// <summary>
+        /// Image and slide description.
+        /// </summary>
+        public string? Description { get; set; }
+    }
+}
