c719bd8da5b9eda721060688d6d8de098ef19b00
Daniel Klement <info@danielklement.net>
2024-11-10 13:49:24 +0100

    New endpoint logic walkthrough

--- Musakai.Blazor/Program.cs
+++ Musakai.Blazor/Program.cs
@@ -7,6 +7,12 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
+var config = new ConfigurationBuilder().SetBasePath(builder.HostEnvironment.BaseAddress)
+    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+    .Build();
+
+builder.Services.AddSingleton<IConfiguration>(config);
+
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 builder.Services.RegisterApiServices();
--- Musakai.Blazor/Services/ApiServices/ApiContactService.cs
+++ Musakai.Blazor/Services/ApiServices/ApiContactService.cs
@@ -7,18 +7,19 @@ namespace Musakai.Blazor.Services.ApiServices;
 /// <summary>
 /// Service for sending contact messages to the Photographer
 /// </summary>
-public class ApiContactService : IApiContactService
+public class ApiContactService : BaseApiService, IApiContactService
 {
-    private readonly HttpClient _httpClient;
-    private Uri _baseEndpoint = new Uri("https://api.danielklement.net/v1/contact");
+    private readonly Uri _endpointV1;
 
     /// <summary>
     /// Constructor for the ApiContactService
     /// </summary>
     /// <param name="httpClient">HttpClient to use for sending requests</param>
-    public ApiContactService(HttpClient httpClient)
+    /// <param name="configuration">Configuration to use for the base endpoint</param>
+    public ApiContactService(HttpClient httpClient, IConfiguration configuration)
+        : base(httpClient, configuration) 
     {
-        _httpClient = httpClient;
+        _endpointV1 = new Uri(_baseEndpoint, "v1/contact");
     }
 
     /// <summary>
@@ -28,7 +29,7 @@ public class ApiContactService : IApiContactService
     /// <returns>Task representing the completion of the request</returns>
     public async Task SendContactMessage(ContactRequest request)
     {
-        var response = await _httpClient.PostAsJsonAsync(new Uri(_baseEndpoint, "send-email"), request);
+        var response = await _httpClient.PostAsJsonAsync(new Uri(_endpointV1,"send-email"), request);
         response.EnsureSuccessStatusCode();
     }
 }
